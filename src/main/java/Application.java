cbd9922a9eae24171ccaa74203f66295dcfc4651
koomchang <leahpar0401@gmail.com>
2023-05-19 15:08:53 +0900

    feat: user model 생성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,15 @@
+import model.User;
+import model.UserRole;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        User user1 = new User("John Doe", UserRole.ADMIN);
+        User user2 = new User("Jane Doe", UserRole.BASIC);
+        User user3 = new User("John Smith", UserRole.BASIC);
+
+        System.out.println("user1 : " + user1.getId());
+        System.out.println("user2 : " + user2.getId());
+        System.out.println("user3 : " + user3.getId());
     }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,26 @@
 package model;
 
 public class User {
+    private static int nextId = 1;
+    private final int id;
+    private final String name;
+    private final UserRole role;
 
+    public User(String name, UserRole role) {
+        this.id = nextId++;
+        this.name = name;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isAdmin() {
+        return role == UserRole.ADMIN;
+    }
 }
