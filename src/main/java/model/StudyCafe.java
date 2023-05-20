67a523d4211fa90c4dd9ab9c7398291efaf237d8
jia5232 <jia5232@naver.com>
2023-05-20 13:48:26 +0900

    feat: studyCafe model 생성

--- src/main/java/model/StudyCafe.java
+++ src/main/java/model/StudyCafe.java
@@ -1,5 +1,26 @@
 package model;
 
-public class StudyCafe {
+import java.util.ArrayList;
+import java.util.List;
 
+public class StudyCafe {
+    private static int nextId = 1;
+    private final int id;
+    private final String name;
+    private final int seatNumber;
+    private final List<Seat> seats;
+
+    public StudyCafe(String name, int seatNumber){
+        this.id = nextId++;
+        this.name = name;
+        this.seatNumber = seatNumber;
+        seats = new ArrayList<>();
+        for(int i=1; i<=seatNumber; i++){
+            seats.add(new Seat(i, true, this));
+        }
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public int getSeatNumber() { return seatNumber; }
 }
