ae2fccea3ff5e91b41b41b5db5c9791f202beb1c
jia5232 <jia5232@naver.com>
2023-05-20 13:48:55 +0900

    feat: seat model 생성

--- src/main/java/model/Seat.java
+++ src/main/java/model/Seat.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Seat {
+    private int id; //스터디카페별 좌석 id
+    private boolean empty;
+    private StudyCafe studyCafe;
 
+    public Seat(int id, boolean empty, StudyCafe studyCafe){
+        this.id = id;
+        this.empty = empty;
+        this.studyCafe = studyCafe;
+    }
+
+    public int getId() { return id; }
+    public boolean isEmpty() { return empty; }
+    public StudyCafe getStudyCafe() { return studyCafe;}
 }
