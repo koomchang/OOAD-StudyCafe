62e716a011db7e4b124dc08e1b68f50bafac8e18
koomchang <leahpar0401@gmail.com>
2023-05-22 03:11:03 +0900

    feat: 현재 유저가 admin이면 스터디카페 등록

--- src/main/java/controller/StudyCafeController.java
+++ src/main/java/controller/StudyCafeController.java
@@ -1,5 +1,19 @@
 package controller;
 
+import model.User;
+import view.StudyCafeView;
+
 public class StudyCafeController {
 
+    private final StudyCafeView studyCafeView = new StudyCafeView();
+
+    public void register(User user) {
+        if (user.isAdmin()) {
+            studyCafeView.printRegister();
+            studyCafeView.askForName();
+            studyCafeView.inputName();
+            studyCafeView.askForSeatAmount();
+            studyCafeView.inputSeatAmount();
+        }
+    }
 }
