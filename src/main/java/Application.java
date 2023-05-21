3838675078091cacb505ceef49acee36872d9ce4
koomchang <leahpar0401@gmail.com>
2023-05-21 16:29:04 +0900

    feat: UserView 에서 user 정보 입출력 관리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,54 +1,52 @@
-
 import model.User;
 import model.UserRole;
-import model.StudyCafe;
-import model.Seat;
-import controller.SeatController;
+import view.UserView;
 
 public class Application {
 
     public static void main(String[] args) {
-        User user1 = new User("John Doe", UserRole.ADMIN);
-        User user2 = new User("Jane Doe", UserRole.BASIC);
-        User user3 = new User("John Smith", UserRole.BASIC);
+        UserView userView = new UserView();
+        userView.start();
+        userView.askForUserName();
+        String userName = userView.inputUserName();
+        userView.askForUserRole();
+        UserRole userRole = userView.inputUserRole();
+        User user = new User(userName, userRole);
+        userView.printUser(user);
 
-        System.out.println("user1 : " + user1.getId());
-        System.out.println("user2 : " + user2.getId());
-        System.out.println("user3 : " + user3.getId());
-        System.out.println("\n\n");
-
-        StudyCafe studyCafe = new StudyCafe("스터디카페1", 10);
-        Seat seat = studyCafe.getSeats().get(0);
+//        StudyCafe studyCafe = new StudyCafe("스터디카페1", 10);
+//        Seat seat = studyCafe.getSeats().get(0);
+////
+//        for (Seat seats : studyCafe.getSeats()) {
+//            System.out.println("생성 후: " + seats.getId() + " : " + seats.isEmpty() + " : "
+//                    + seats.getStudyCafe().getName());
+//        }
 //
-        for(Seat seats : studyCafe.getSeats()){
-            System.out.println("생성 후: " + seats.getId() + " : " + seats.isEmpty() + " : " + seats.getStudyCafe().getName());
-        }
-
-        SeatController seatController = new SeatController(seat);
-        seatController.reserveSeat(seat.getId()-1);
-
-        System.out.println("\n\n");
-
-        for(Seat seats : studyCafe.getSeats()){
-            System.out.println("예약 후: " + seats.getId() + " : " + seats.isEmpty() + " : " + seats.getStudyCafe().getName());
-        }
-
-        seatController.changeSeat(0, 1);
-
-        System.out.println("\n\n");
-
-
-        for(Seat seats : studyCafe.getSeats()){
-            System.out.println("변경 후: " + seats.getId() + " : " + seats.isEmpty() + " : " + seats.getStudyCafe().getName());
-        }
-
-        seatController.closeSeat(1);
-
-        System.out.println("\n\n");
-
-
-        for(Seat seats : studyCafe.getSeats()){
-            System.out.println("종료 후: " + seats.getId() + " : " + seats.isEmpty() + " : " + seats.getStudyCafe().getName());
-        }
+//        SeatController seatController = new SeatController(seat);
+//        seatController.reserveSeat(seat.getId() - 1);
+//
+//        System.out.println("\n\n");
+//
+//        for (Seat seats : studyCafe.getSeats()) {
+//            System.out.println("예약 후: " + seats.getId() + " : " + seats.isEmpty() + " : "
+//                    + seats.getStudyCafe().getName());
+//        }
+//
+//        seatController.changeSeat(0, 1);
+//
+//        System.out.println("\n\n");
+//
+//        for (Seat seats : studyCafe.getSeats()) {
+//            System.out.println("변경 후: " + seats.getId() + " : " + seats.isEmpty() + " : "
+//                    + seats.getStudyCafe().getName());
+//        }
+//
+//        seatController.closeSeat(1);
+//
+//        System.out.println("\n\n");
+//
+//        for (Seat seats : studyCafe.getSeats()) {
+//            System.out.println("종료 후: " + seats.getId() + " : " + seats.isEmpty() + " : "
+//                    + seats.getStudyCafe().getName());
     }
 }
--- src/main/java/view/UserView.java
+++ src/main/java/view/UserView.java
@@ -1,2 +1,100 @@
-package view;public class UserView {
+package view;
+
+import exception.UserNameException;
+import exception.UserRoleException;
+import java.util.Scanner;
+import model.User;
+import model.UserRole;
+
+public class UserView {
+
+    private final Scanner sc = new Scanner(System.in);
+
+    public void start() {
+        System.out.println("스터디카페에 오신 것을 환영합니다!");
+    }
+
+    public void askForUserName() {
+        System.out.println("환영합니다! 본인의 이름을 입력해주세요.");
+    }
+
+    public void askForUserRole() {
+        System.out.println("본인의 역할을 입력해주세요. (1: 스터디카페 관리자, 2: 스터디카페 손님)");
+    }
+
+    public void printUser(User user) {
+        System.out.println("환영합니다! " + user.getName() + "님");
+        if (user.isAdmin()) {
+            System.out.println("관리자로 로그인하셨습니다.");
+        } else {
+            System.out.println("손님으로 로그인하셨습니다.");
+        }
+    }
+
+    public String inputUserName() {
+        boolean isUserNameValid = false;
+        String userName = null;
+        do {
+            userName = sc.nextLine();
+            try {
+                validateUserName(userName);
+                isUserNameValid = true;
+            } catch (UserNameException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!isUserNameValid);
+        return userName;
+    }
+
+    public UserRole inputUserRole() {
+        boolean isUserRoleValid = false;
+        UserRole userRole = null;
+        int userRoleInput = 0;
+        do {
+            String userInput = sc.nextLine();
+            try {
+                validateUserRole(userInput);
+                userRoleInput = Integer.parseInt(userInput);
+                isUserRoleValid = true;
+            } catch (UserRoleException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!isUserRoleValid);
+        if (userRoleInput == 1) {
+            userRole = UserRole.ADMIN;
+        } else if (userRoleInput == 2) {
+            userRole = UserRole.BASIC;
+        }
+        return userRole;
+    }
+
+    public void validateUserRole(String userInput) {
+        validateInputNumeric(userInput);
+        validateUserRoleValue(userInput);
+    }
+
+    public void validateUserRoleValue(String userInput) {
+        int userRole = Integer.parseInt(userInput);
+        if (userRole != 1 && userRole != 2) {
+            throw new UserRoleException("1 또는 2를 입력해주세요.");
+        }
+    }
+
+    public void validateInputNumeric(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new UserRoleException("숫자만 입력 가능합니다.");
+        }
+    }
+
+    public void validateUserName(String userName) {
+        validateExist(userName);
+    }
+
+    public void validateExist(String userName) {
+        if (userName.isEmpty()) {
+            throw new UserNameException("이름은 공백일 수 없습니다");
+        }
+    }
 }
