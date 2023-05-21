0a65b52445b7984875e01f8b90c0e3e87c7f8c98
koomchang <leahpar0401@gmail.com>
2023-05-22 03:09:46 +0900

    feat: StudyCafe View 구현(validation 구현 필요)

--- src/main/java/view/StudyCafeView.java
+++ src/main/java/view/StudyCafeView.java
@@ -9,4 +9,26 @@ public class StudyCafeView {
     public void start() {
         System.out.println("스터디카페에 오신 것을 환영합니다!");
     }
+
+    public void printRegister() {
+        System.out.println("스터디카페를 등록합니다");
+    }
+
+    public void askForName() {
+        System.out.println("스터디카페 이름을 입력해주세요.");
+    }
+
+    // TODO: input validation
+    public String inputName() {
+        return sc.nextLine();
+    }
+
+    public void askForSeatAmount() {
+        System.out.println("스터디카페 총 좌석 수를 입력해주세요.");
+    }
+
+    // TODO: input validation
+    public int inputSeatAmount() {
+        return sc.nextInt();
+    }
 }
