b1f60eb331665cda78a553a0f50b6a1d8349c77a
koomchang <leahpar0401@gmail.com>
2023-05-22 03:10:28 +0900

    refactor: Application에 있던 내용을 UserController 에서 처리

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,2 +1,20 @@
-package controller;public class UserController {
+package controller;
+
+import model.User;
+import model.UserRole;
+import view.UserView;
+
+public class UserController {
+
+    private final UserView userView = new UserView();
+
+    public User register() {
+        userView.askForName();
+        String userName = userView.inputName();
+        userView.askForRole();
+        UserRole userRole = userView.inputRole();
+        User user = new User(userName, userRole);
+        userView.printUserInfo(user);
+        return user;
+    }
 }
