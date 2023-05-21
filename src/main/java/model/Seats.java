9b5666aede49ae3fd6e92d0ae140012c1e85fe80
koomchang <leahpar0401@gmail.com>
2023-05-22 03:07:40 +0900

    refactor: StudyCafe model에 있던 seats를 일급컬렉션으로 구현

--- src/main/java/model/Seats.java
+++ src/main/java/model/Seats.java
@@ -1,2 +1,33 @@
-package model;public class Seats {
+package model;
+
+import exception.SeatReservationException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Seats {
+
+    private final List<Seat> seats;
+
+    public Seats(int seatNumber) {
+        this.seats = IntStream.range(1, seatNumber + 1)
+                .mapToObj(i -> new Seat(i, null))
+                .collect(Collectors.toList());
+    }
+
+    public Seat getSeatBySeatNumber(int seatNumber) {
+        return seats.stream()
+                .filter(seat -> seat.getSeatNumber() == seatNumber)
+                .findFirst()
+                .orElse(null);
+    }
+
+    //
+    public void closeSeat(User user) {
+        Seat closingSeat = seats.stream()
+                .filter(seat -> seat.getUser().equals(user))
+                .findFirst()
+                .orElseThrow(() -> new SeatReservationException("아직 좌석 예약을 하지 않으셨습니다"));
+        closingSeat.removeSeatFromUser(user);
+    }
 }
