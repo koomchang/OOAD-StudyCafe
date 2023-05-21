5e7645bdea57fd9eafacb6c4c96428c10faece47
koomchang <leahpar0401@gmail.com>
2023-05-22 03:04:42 +0900

    refactor: UserRole이 roleNumber를 속성값으로 가지도록 설정

--- src/main/java/model/UserRole.java
+++ src/main/java/model/UserRole.java
@@ -1,6 +1,23 @@
 package model;
 
+import exception.UserRoleException;
+
 public enum UserRole {
-    ADMIN,
-    BASIC
+    ADMIN(1),
+    BASIC(2);
+
+    private final int roleNumber;
+
+    UserRole(int roleNumber) {
+        this.roleNumber = roleNumber;
+    }
+
+    public static UserRole of(int roleNumber) {
+        if (roleNumber == 1) {
+            return ADMIN;
+        } else if (roleNumber == 2) {
+            return BASIC;
+        }
+        throw new UserRoleException("1 또는 2를 입력해주세요.");
+    }
 }
