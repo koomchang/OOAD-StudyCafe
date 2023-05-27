f45066ee66ebca9fb9cd8e3d7415975e8080f187
Mumat0103 <wsx2138@naver.com>
2023-05-27 11:33:30 +0900

    feat : Make Review model

--- src/main/java/model/Review.java
+++ src/main/java/model/Review.java
@@ -1,5 +1,33 @@
 package model;
 
-public class Review {
+import java.util.Date;
 
+public class Review {
+    private int reviewId;
+    private final User author;
+    private final Date createDate;
+    private final String content;
+
+    public Review(User author, String content) {
+        this.reviewId = reviewId + 1;
+        this.author = author;
+        this.createDate = new Date();
+        this.content = content;
+    }
+
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
